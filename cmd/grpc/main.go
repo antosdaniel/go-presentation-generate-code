95ac8333d9d5a63ef650ac5c9e31cc604b1e9a81
Daniel Antos <antosdaniel@gmail.com>
2023-03-12 18:00:02 +0100

    fix: graceful shutdown

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
+	"net/http"
 
 	"github.com/antosdaniel/go-presentation-generate-code/internal/grpc"
 	"github.com/antosdaniel/go-presentation-generate-code/internal/shutdown"
@@ -12,15 +14,23 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	shutdown.SetupGraceful(ctx, cancel)
-	server := grpc.Setup()
-	defer server.Shutdown(ctx)
+	server := setup(ctx, cancel)
 
 	log.Println("starting server...")
 	err := server.ListenAndServe()
-	if err != nil {
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatalf("unable to start gRPC server: %v", err) //nolint:gocritic
 	}
 
 	<-ctx.Done() // Wait for shutdown signal.
+}
+
+func setup(ctx context.Context, cancel context.CancelFunc) *http.Server {
+	server := grpc.Setup()
+
+	shutdown.SetupGraceful(ctx, cancel, shutdown.Closers{
+		"grpc server": func() error { return server.Shutdown(ctx) },
+	})
+
+	return server
 }
--- internal/shutdown/graceful.go
+++ internal/shutdown/graceful.go
@@ -8,7 +8,11 @@ import (
 	"syscall"
 )
 
-func SetupGraceful(ctx context.Context, cancel context.CancelFunc) {
+type ComponentName string
+
+type Closers map[ComponentName]func() error
+
+func SetupGraceful(ctx context.Context, cancel context.CancelFunc, closers Closers) {
 	quitCh := make(chan os.Signal, 1)
 	// interrupt signal sent from terminal
 	signal.Notify(quitCh, syscall.SIGINT)
@@ -19,5 +23,18 @@ func SetupGraceful(ctx context.Context, cancel context.CancelFunc) {
 		sig := <-quitCh
 		log.Printf("received interrupt %q, shutting down...", sig)
 		cancel()
+		closeAll(closers)
 	}()
+}
+
+func closeAll(closers Closers) {
+	for name, c := range closers {
+		if c == nil {
+			continue
+		}
+		err := c()
+		if err != nil {
+			log.Printf("unable to close %q", name)
+		}
+	}
 }
